17a60f9c3f2ae7d8d28726f8d599a1f5c41198b5
Хозяинов Александр Владимирович <Aleksandr.Khozyainov@rosbank.ru>
2022-01-15 16:51:25 +0300

    uncomment DB

--- cmd/dev_server/main.go
+++ cmd/dev_server/main.go
@@ -7,6 +7,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"database/sql"
 	"encoding/pem"
 	"expvar"
 	"flag"
@@ -63,12 +64,12 @@ func main() {
 		errLogger.Fatal("Failed to make TLS key or cert: ", err)
 	}
 
-	// d, err := sql.Open("postgres", "user=dobi dbname=knox")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// db, err := keydb.NewPostgreSQLDB(d)
-	db := keydb.NewTempDB()
+	d, err := sql.Open("postgres", "user=dobi dbname=knox")
+	if err != nil {
+		errLogger.Fatal("SQL Failed: ", err)
+	}
+	db, err := keydb.NewPostgreSQLDB(d)
+	// db := keydb.NewTempDB()
 
 	server.AddDefaultAccess(&knox.Access{
 		Type:       knox.UserGroup,
